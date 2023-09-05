44a54d0ba4944dcc5177b8f7aaa4a483c966fef8
Jiarung02 <140510029+Jiarung02@users.noreply.github.com>
2023-09-05 12:57:31 +0000

    modify

--- lesson13_2.py
+++ lesson13_2.py
@@ -1,3 +1,9 @@
 import student
-stu1 = student.Student("吳家榕",66,69,78)
+
+stu1 = student.get_student("徐國堂")
 print(stu1.chinese)
+print(stu1.english)
+print(stu1.math)
+print(stu1.total())
+print(stu1.average)
+print(stu1)
