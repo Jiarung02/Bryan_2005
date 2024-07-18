10a1e4a5552bd3f9ed8eababf4d0c5d999e5329d
Jiarung02 <140510029+Jiarung02@users.noreply.github.com>
2024-07-18 14:09:28 +0000

    modify

--- lesson13_2.py
+++ lesson13_2.py
@@ -1,9 +1,9 @@
 import student
 
-stu1 = student.get_student("吳家榕")  #module名稱 /＃class名稱
+stu1 = student.Student("吳家榕", 86, 91, 74)  #module名稱 /＃class名稱
 print(stu1.chinese)#實體attribute
 print(stu1.english)
 print(stu1.math)
 print(stu1.total()) #實體method
-print(stu1.average)#實體property
+print(stu1.average)#實體property不括號
 print(stu1)
